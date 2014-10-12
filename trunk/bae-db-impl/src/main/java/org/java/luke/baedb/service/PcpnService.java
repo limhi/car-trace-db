0eb32c6ee8b78b23b3f5d5fb011d21fbe37af43c
limhi.cpc@gmail.com <limhi.cpc@gmail.com@20f52aa0-c404-17e0-4906-09fcf7146b0a>
2014-10-12 14:45:10 +0000

    setup db

---
+++ trunk/bae-db-impl/src/main/java/org/java/luke/baedb/service/PcpnService.java
@@ -0,0 +1,56 @@
+package org.java.luke.baedb.service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.java.luke.baedb.dao.PcpnMapper;
+import org.java.luke.baedb.model.Pcpn;
+import org.java.luke.baedb.model.PcpnCriteria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PcpnService {
+	@Autowired
+	private PcpnMapper mapper;
+
+	public PcpnService() {
+	}
+
+	public int countByExample(PcpnCriteria example) throws Exception {
+		return mapper.countByExample(example);
+	}
+
+	// @Transactional
+	public int insert(Pcpn record) throws Exception {
+		return mapper.insertSelective(record);
+	}
+
+	// @Transactional
+	public int deleteByExample(PcpnCriteria example) throws Exception {
+		return mapper.deleteByExample(example);
+	}
+
+	// @Transactional
+	public int updateByExample(Pcpn record, PcpnCriteria example) throws Exception {
+		return mapper.updateByExampleSelective(record, example);
+	}
+
+	public List<Pcpn> selectByExample(PcpnCriteria example, int record_start, int record_stop) throws Exception {
+		if (record_stop >= record_start) {
+			return mapper.selectByExample(example, record_start, record_stop, (record_stop - record_start + 1));
+		} else {
+			return new LinkedList<Pcpn>();
+		}
+	}
+
+	public List<Pcpn> selectByExample(PcpnCriteria example) throws Exception {
+		int record_start = 0;
+		int record_stop = countByExample(example) - 1;
+		return selectByExample(example, record_start, record_stop);
+	}
+  
+  public int flushCache(){
+    return mapper.flushCache();
+  }
+}
---
+++ trunk/bae-db-impl/src/main/java/org/java/luke/baedb/service/PnmessageService.java
@@ -0,0 +1,56 @@
+package org.java.luke.baedb.service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.java.luke.baedb.dao.PnmessageMapper;
+import org.java.luke.baedb.model.Pnmessage;
+import org.java.luke.baedb.model.PnmessageCriteria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PnmessageService {
+	@Autowired
+	private PnmessageMapper mapper;
+
+	public PnmessageService() {
+	}
+
+	public int countByExample(PnmessageCriteria example) throws Exception {
+		return mapper.countByExample(example);
+	}
+
+	// @Transactional
+	public int insert(Pnmessage record) throws Exception {
+		return mapper.insertSelective(record);
+	}
+
+	// @Transactional
+	public int deleteByExample(PnmessageCriteria example) throws Exception {
+		return mapper.deleteByExample(example);
+	}
+
+	// @Transactional
+	public int updateByExample(Pnmessage record, PnmessageCriteria example) throws Exception {
+		return mapper.updateByExampleSelective(record, example);
+	}
+
+	public List<Pnmessage> selectByExample(PnmessageCriteria example, int record_start, int record_stop) throws Exception {
+		if (record_stop >= record_start) {
+			return mapper.selectByExample(example, record_start, record_stop, (record_stop - record_start + 1));
+		} else {
+			return new LinkedList<Pnmessage>();
+		}
+	}
+
+	public List<Pnmessage> selectByExample(PnmessageCriteria example) throws Exception {
+		int record_start = 0;
+		int record_stop = countByExample(example) - 1;
+		return selectByExample(example, record_start, record_stop);
+	}
+  
+  public int flushCache(){
+    return mapper.flushCache();
+  }
+}
